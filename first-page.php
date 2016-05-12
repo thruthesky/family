122b650852c3c4afbaa55612d58fe2cee0a118fe
thruthesky <thruthesky@gmail.com>
2016-05-12 13:23:49 +0900

    Updating front page

--- first-page.php
+++ first-page.php
@@ -4,7 +4,7 @@ wp_enqueue_style("first-page", get_template_directory_uri() . '/css/first-page.c
 <div class="row">
     <?php
     $args = array(
-        'numberposts' => 12,
+        'numberposts' => 13,
         'offset' => 0,
         'category' => 0,
         'orderby' => 'post_date',
@@ -13,7 +13,32 @@ wp_enqueue_style("first-page", get_template_directory_uri() . '/css/first-page.c
         'post_status' => 'publish'
     );
     $recent_posts = wp_get_recent_posts( $args, ARRAY_A );
+    $shift = array_shift($recent_posts);
+    setup_postdata($shift);
+    ?>
+    <style scoped>
+    </style>
+    <div class="first-post">
+        <a href="<?php echo get_permalink( get_the_ID() )?>">
+        <?php
+        $src = forum()->get_first_image( get_the_ID() );
+        if ( $src ) {
+            ?>
+            <div class="photo">
+                    <img src='<?php echo $src?>'>
+            </div>
+            <?php
+        }
+        ?>
+        <div class="title"><?php the_title()?></div>
+            <div class="desc">
+                <?php echo mb_strcut(strip_tags( get_the_content() ), 0, 180 * 3);?>
+            </div>
+            </a>
+    </div>
+    <?php
     foreach( $recent_posts as $recent ) {
+        setup_postdata($recent);
         ?>
         <div class="first-page-post col-xs-6 col-lg-4">
             <h2 class="first-page-post-title">
@@ -31,6 +56,13 @@ wp_enqueue_style("first-page", get_template_directory_uri() . '/css/first-page.c
                 <?php
             }
             ?>
+            <div class="first-page-post-info">
+                <span class="date"><?php echo lib()->date_short($recent["post_date"])?></span>
+                <span class="author">
+                by
+                    <?php the_author() ?>
+                    </span>
+            </div>
             <div class="first-page-post-desc">
                 <a href="<?php echo get_permalink($recent["ID"])?>"><?php echo mb_strcut(strip_tags($recent['post_content']), 0, 3 * 88);?></a>
             </div>
